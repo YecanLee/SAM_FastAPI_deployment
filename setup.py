0ba8302ee34eba1b78e9a73895c93042601e5510
Li <M.Li@campus.lmu.de>
2023-10-14 18:19:18 +0200

    Initialization of the structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='a simple deployment of the segment anything model by using fastapi library in python',
+    author='scout',
+    license='MIT',
+)
